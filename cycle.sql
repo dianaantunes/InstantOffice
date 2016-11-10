981b35b0170e845c18d087bcdb0bfb200f5374e6
Eduardo Janicas <edujanicas@icloud.com>
2016-11-10 10:03:27 +0000

    Removed cycle.sql

--- cycle.sql
+++
@@ -1,8 +0,0 @@
-DO
-$do$
-BEGIN -- BEGIN ISOLATION LEVEL SERIALIZABLE;
-FOR i IN 1..1000 LOOP
-   DELETE FROM reserva WHERE reserva.numero IN (SELECT numero FROM reserva LIMIT 1000);
-END LOOP;
-END -- COMMIT
-$do$;
