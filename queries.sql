e1f51633326d7d0beb88d235a7e30ceab4bd4785
Eduardo Janicas <edujanicas@icloud.com>
2016-12-01 12:37:13 +0000

    Some changes made

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT morada, codigo_espaco FROM posto p
 WHERE NOT EXISTS (SELECT morada FROM aluga a WHERE p.codigo = a.codigo);
 
 -- b)
-SELECT morada AS 'Numero de reservas superior a media' FROM aluga NATURAL JOIN reserva
+SELECT morada FROM aluga NATURAL JOIN reserva
 GROUP BY morada
 HAVING COUNT(morada) >= (
 	SELECT AVG(temp.count) FROM (
@@ -19,20 +19,12 @@ NATURAL JOIN(
   SELECT nif FROM aluga NATURAL JOIN fiscaliza
   GROUP BY nif
   HAVING COUNT(DISTINCT id) = 1
-) AS cenas;
--- SELECT nome
--- FROM user NATURAL JOIN aluga NATURAL JOIN alugavel NATURAL JOIN fiscaliza
--- NATURAL JOIN fiscal
--- GROUP BY nome
--- HAVING COUNT(fiscal.id) < ALL (
--- 	SELECT COUNT(fiscal.id) FROM fiscal
--- 	GROUP BY nome
--- );
+) AS temp;
 
 -- d)
-SELECT 365 * SUM(tarifa) AS 'Montante total realizado'
+SELECT dias * SUM(tarifa) AS 'Total'
 FROM alugavel NATURAL JOIN
-(SELECT * FROM oferta WHERE data_inicio < '2016-12-31' AND data_fim > '2016-01-01') as temp
+(SELECT DATEDIFF(data_inicio,data_fim) AS dias, morada, codigo, tarifa FROM oferta WHERE data_inicio < '2016-12-31' AND data_fim > '2016-01-01') as temp
 GROUP BY morada, codigo;
 
 -- e)
@@ -42,14 +34,5 @@ NATURAL JOIN(
 	SELECT numero, estado FROM aluga NATURAL JOIN estado
 	GROUP BY numero
 	HAVING estado = 'aceite'
-) AS cenas;
+) AS temp;
 
-
--- SELECT morada, codigo
--- FROM posto NATURAL JOIN aluga
--- GROUP BY morada
--- HAVING COUNT(morada) < ALL (
--- 	SELECT codigo FROM posto AS p NATURAL JOIN aluga AS a
--- 	WHERE p.codigo = a.codigo
--- 	GROUP BY morada
--- );
--- storedprocstriggers.sql
+++ storedprocstriggers.sql
@@ -8,7 +8,8 @@ BEGIN
 	IF NEW.data < ANY (SELECT timestamp FROM
 					estado
 					WHERE estado.numero = new.numero)
-	THEN SET NEW.data = CURRENT_DATE; -- Caso contrario coloca a data atual
+	THEN signal sqlstate '45000' set message_text = 'A data de pagamento de uma reserva paga tem de ser superior ao timestamp do
+ultimo estado dessa reserva';
 	END IF;
 END //
 delimiter ;
